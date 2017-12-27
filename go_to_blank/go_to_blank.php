c05159e9ad4f88724ba90cf6a65714aa60e480ca
Constantin Guay <const.g@gmail.com>
2017-12-27 15:38:53 +0100

    Do not modify # links or relative links and add a class to be targeted by css in the theme

--- go_to_blank/go_to_blank.php
+++ go_to_blank/go_to_blank.php
@@ -4,10 +4,12 @@ Plugin Name: Go To Blank
 Plugin URI: https://const.fr/gotoblank
 Description: Force external links to open in a new tab/window
 Author: Constantin Guay
-Version: 1.0.1
+Version: 1.1.0
 Author URI: https://const.fr
 
-Changelog : 
+Changelog :
+    1.1.0 : . Do not modify # links or relative links
+            . Add a class to be target by css in the theme
     1.0.1 : . Removed the .add() who were not needed.
     1.0.0 : . First beta
 */
@@ -17,8 +19,9 @@ function add_blank_script() {
     $this_domain = get_option('siteurl');
     ?>
     <script>
-        jQuery('a[href^="http"]:not([href^="<?php echo($this_domain); ?>"])') 
-            .attr('target','_blank');
+        jQuery('a[href^="http"]:not( [href^="<?php echo($this_domain); ?>"] ):not( [href^=\'#\'] ):not( [href^=\'/\'] )')
+            .attr('target','_blank')
+            .addClass('external-link');
     </script>
     <?php
 }
