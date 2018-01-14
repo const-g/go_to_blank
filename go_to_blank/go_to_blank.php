1a31f83a29c5d9584f261da8578f2c14fe9ffe23
Constantin Guay <const.g@gmail.com>
2018-01-14 12:33:49 +0100

    add noopener to protect external links

--- go_to_blank/go_to_blank.php
+++ go_to_blank/go_to_blank.php
@@ -1,15 +1,17 @@
 <?php
 /*
-Plugin Name: Go To Blank
+Plugin Name: Cog_g's Go To Blank
 Plugin URI: https://const.fr/gotoblank
 Description: Force external links to open in a new tab/window
 Author: Constantin Guay
-Version: 1.1.0
+Version: 1.2.0
 Author URI: https://const.fr
 
 Changelog :
+    1.2.0 : . add noopener to protect external links
+            . more info on https://mathiasbynens.github.io/rel-noopener/
     1.1.0 : . Do not modify # links or relative links
-            . Add a class to be target by css in the theme
+            . Add an class to be target by css in the theme
     1.0.1 : . Removed the .add() who were not needed.
     1.0.0 : . First beta
 */
@@ -21,6 +23,7 @@ function add_blank_script() {
     <script>
         jQuery('a[href^="http"]:not( [href^="<?php echo($this_domain); ?>"] ):not( [href^=\'#\'] ):not( [href^=\'/\'] )')
             .attr('target','_blank')
+            .attr('rel', 'noopener')
             .addClass('external-link');
     </script>
     <?php
